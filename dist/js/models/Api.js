3bccc970eec148bba07a4ebe3376ed84536baed5
GoDari <godfred@xpathire.com>
2022-11-14 14:43:20 +0000

    updated

--- dist/js/models/Api.js
+++ dist/js/models/Api.js
@@ -1,14 +1,19 @@
 export default class {
-  
   constructor(axios) {
     this.axiosClient = axios.create();
     this.axiosClient.defaults.baseURL = "https://freddy.codesubmit.io";
+    this.axiosClient.interceptors.response.use(
+      this.successInterceptor,
+      this.errorHandleInterceptor
+    );
   }
 
-  token = (token="") => {
-    this.axiosClient.defaults.headers.common['Authorization'] = `Bearer ${token}`
-    return this
-  }
+  token = (token = "") => {
+    this.axiosClient.defaults.headers.common[
+      "Authorization"
+    ] = `Bearer ${token}`;
+    return this;
+  };
 
   get = async (url) => {
     return this.axiosClient.get(url);
@@ -18,7 +23,13 @@ export default class {
     return this.axiosClient.post(url, data);
   };
 
-  configuration = () => ({
-    baseURL: "https://freddy.codesubmit.io",
-  });
+  successInterceptor = success => {
+    return Promise.resolve(success)
+  };
+  errorHandleInterceptor = error => {
+    if(error.response){
+      alert(error.response.data?.msg)
+    }
+    return Promise.reject(error)
+  };
 }
